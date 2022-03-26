aed1e9e4daccfca93b47c5f8c1543a63cc047c22
mhill44 <90353090+mhill44@users.noreply.github.com>
2022-03-26 15:14:17 -0500

    Create 1-export_to_CSV.py

---
+++ 0x0E-api/1-export_to_CSV.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+This extends the Python script in task 0
+to export data into CSV format (not JSON)
+"""
+import csv
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    R = requests.get('https://jsonplaceholder.typicode.com/users/{:}'
+                     .format(argv[1])).json()
+    R_two = requests.get(
+        'https://jsonplaceholder.typicode.com/todos/?userId={:}'
+        .format(argv[1])).json()
+
+    userID = argv[1]
+    name = R.get('username')
+    with open('{:}.csv'.format(argv[1]), mode='w') as user_id_file:
+        user_writer = csv.writer(user_id_file, quoting=csv.QUOTE_ALL)
+        for task in R_two:
+            user_writer.writerow([userID, name, task.get('completed'),
+                                  task.get('title')])
